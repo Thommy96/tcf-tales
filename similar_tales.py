daf1b68afaa80c35a77b1947435aceecc63c37b6
Thomas Bott <thomas_bott@outlook.de>
2021-02-26 17:01:52 +0100

    add ovelapping nouns to output

--- similar_tales.py
+++ similar_tales.py
@@ -11,16 +11,18 @@ if __name__ == "__main__":
         a_list = a.split(',')
         b_list = b.split(',')
         similarity = len(set(a_list) & set(b_list))
+        sim_nouns = set(a_list) & set(b_list)
+        sim_nouns = ', '.join(str(n) for n in sim_nouns)
         index_a = top_list.index(a)
         index_b = top_list.index(b)
         title_a = stats_df['title'][index_a]
         title_b = stats_df['title'][index_b]
-        tale_similarity[(title_a, title_b)] = similarity
+        tale_similarity[(title_a, title_b, sim_nouns)] = similarity
     
     tale_similarity = dict(sorted(tale_similarity.items(), key=lambda item: item[1], reverse=True))
     
     with open('similar_tales.tsv', 'wt') as f:
         tsv_writer = csv.writer(f, delimiter="\t")
-        tsv_writer.writerow(['tales', 'similarity (overlap of top 10 nouns)'])
+        tsv_writer.writerow(['tales', 'similarity (overlap of top 10 nouns)', 'nouns'])
         for tales, sim in tale_similarity.items():
-            tsv_writer.writerow([tales, sim])
+            tsv_writer.writerow([tales[0] + ', ' + tales[1], sim, tales[2]])
